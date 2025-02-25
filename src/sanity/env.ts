ac078a4187fa9efc7992628fe6bca48198e9b876
Bruno MacM3 <wbrunovieira77@gmail.com>
2025-02-25 19:43:56 -0300

    fixed env load added in env.ts for now

--- src/sanity/env.ts
+++ src/sanity/env.ts
@@ -1,15 +1,21 @@
-export const apiVersion =
-  process.env.VITE_SANITY_API_VERSION || '2025-02-24';
+export const apiVersion = '2025-02-24';
 
 export const dataset = assertValue(
-  process.env.VITE_SANITY_DATASET,
-  'Missing environment variable: VITE_SANITY_DATASET'
+  'production',
+  'Missing environment variable: NEXT_PUBLIC_SANITY_DATASET'
 );
 
 export const projectId = assertValue(
-  process.env.VITE_SANITY_PROJECT_ID,
-  'Missing environment variable: VITE_SANITY_PROJECT_ID'
+  'e2glo6ep',
+  'Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID'
 );
+
+console.log(
+  'Sanity Project ID:',
+  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
+);
+console.log('Sanity Dataset:', 'production');
+console.log('Sanity API Version:', '2025-02-24');
 
 function assertValue<T>(
   v: T | undefined,
