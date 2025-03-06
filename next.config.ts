cce96b45242bf36682eb37f30ba7662adc1d85cb
Bruno MacM3 <wbrunovieira77@gmail.com>
2025-03-06 07:19:52 -0300

    ädded headers

--- next.config.ts
+++ next.config.ts
@@ -5,4 +5,22 @@ module.exports = withNextIntl({
   images: {
     domains: ['cdn.sanity.io'],
   },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          { key: 'X-Frame-Options', value: 'DENY' },
+          {
+            key: 'X-XSS-Protection',
+            value: '1; mode=block',
+          },
+        ],
+      },
+    ];
+  },
 });
